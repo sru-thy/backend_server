a27f9ba1b5042ae8a86af50f091b0555c27eabc1
Sruthy M L <sruthyml328@gmail.com>
2023-08-05 17:45:18 +0530

    extend abstract entity to address entity

--- src/entity/address.entity.ts
+++ src/entity/address.entity.ts
@@ -1,33 +1,20 @@
 import {
   Column,
-  CreateDateColumn,
-  DeleteDateColumn,
   Entity,
   JoinColumn,
-  ManyToOne,
   OneToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
 } from "typeorm";
 import Employee from "./employee.entity";
+import { AbstractEntity } from "./abstract-entity";
 
 @Entity()
-export class Address {
-  @PrimaryGeneratedColumn()
-  id: number;
+export class Address extends AbstractEntity {
   @Column()
   line1: string;
   @Column()
   line2: string;
   @Column()
   pincode: string;
-  @CreateDateColumn()
-  createdAt: Date;
-  @UpdateDateColumn()
-  updatedAt: Date;
-  @DeleteDateColumn()
-  deletedAt: Date;
-
   @OneToOne(() => Employee, (employee) => employee.address)
   @JoinColumn()
   employee: Employee;
